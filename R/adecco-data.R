0cf0e611c553c766a8deee2faa4a673996ccdaaf
KarolisKoncevicius <karolis.koncevicius@gmail.com>
2019-08-30 01:24:02 +0300

    added adecco data

---
+++ R/adecco-data.R
@@ -0,0 +1,19 @@
+#' Adecco Swiss Job Market Index Data
+#'
+#' Adecco Swiss job market index dataset in `swissdata` form
+#' taken from the University of Zürich.
+#'
+#' @docType data
+#'
+#' @usage data(adecco)
+#'
+#' @keywords datasets
+#'
+#' @source \href{University of Zürich}{University of Zürich}
+#'
+#' @examples
+#' data(adecco)
+#' adecco$data
+#' adecco$meta$hierarchy
+#' adecco$meta$labels
+"adecco"
