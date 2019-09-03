a31451cea6fc6308e80c9dd7cdda5d4d7ded9a58
KarolisKoncevicius <karolis.koncevicius@gmail.com>
2019-09-04 02:27:59 +0300

    added rfind to utils.R

--- R/utils.R
+++ R/utils.R
@@ -6,3 +6,17 @@ ensure_path <- function(path) {
   file.remove(list.files(path, full.names = TRUE))
 }
 
+# Recursive Find in a List
+# x <- list(d = list(c = list(a = "aa", b = "bb")), e = list(f = NA))
+# rfind(x, "a")
+# rfind(x, "f")
+rfind <- function(x, name, pos=c()) {
+  ind <- match(name, names(x))
+  if (!is.na(ind)) return(c(pos, ind))
+  for (i in seq_along(x)) {
+    if (class(x[i]) == "list") {
+      out <- Recall(x[[i]], name, pos)
+      if (!is.null(out)) return(c(i, out))
+    }
+  }
+}
