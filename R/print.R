8ef0c2aa94539dddb4ce958ca52ecd00b897916e
KarolisKoncevicius <karolis.koncevicius@gmail.com>
2019-09-04 02:11:51 +0300

    replaced the str.swissdata.R with custom implementation and made print.R call str

--- R/print.R
+++ R/print.R
@@ -3,36 +3,25 @@
 #' Function for printing swissdata objects
 #'
 #' @param x swissdata object
+#' @param language language to be used for printing the labels
 #'
 #' @return invisibly returns the swissdata object
+#'
+#' @examples
+#' z <- adecco
+#' print(z)
+#' print(z, languge = "de")
+#'
+#' @seealso `str.swissdata`
 #'
 #' @author Christoph Sax
 #' @export
 print.swissdata <- function(x, language = 'en') {
 
-  meta <- x$meta
-  labels <- x$meta$labels
-  hierarchy <- meta$hierarchy
+  print(head(x$data))
+  cat("\n")
+  str(x, language)
 
-  # if a dim is missing in hierarchy, use names from labels
-  dims.no.covered <- setdiff( names(labels), c(names(hierarchy), "dimnames"))
-  hierarchy <- c(hierarchy, lapply(labels[dims.no.covered], function(x) lapply(x, function(x) list())))
-
-  label.hierarchy <- hierarchy
-  for(dm in setdiff(names(labels), "dimnames")) {
-    pos <- rfind(label.hierarchy, dm)
-    lab <- paste(dm, "-", labels$dimnames[[dm]][[language]])
-    label.hierarchy[[pos]] <- list(lab, label.hierarchy[[pos]])
-    for(lv in names(labels[[dm]])) {
-      pos <- rfind(label.hierarchy, lv)
-      pad <- (length(rfind(hierarchy, lv)) - 1) * 2
-      lab <- paste(lv, "-", labels[[dm]][[lv]][[language]])
-      lab <- paste0(strrep(" ", pad), lab)
-      label.hierarchy[[pos]] <- list(lab, label.hierarchy[[pos]])
-    }
-  }
-
-  cat(paste0(unlist(label.hierarchy), "\n", collapse=""))
   invisible(x)
 }
 
