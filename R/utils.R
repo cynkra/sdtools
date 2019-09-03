618e19b3a2ae4d73b19e41372063fa07b107ebb5
KarolisKoncevicius <karolis.koncevicius@gmail.com>
2019-09-04 02:00:04 +0300

    added documentation for marry.R

--- R/utils.R
+++ R/utils.R
@@ -7,6 +7,9 @@ ensure_path <- function(path) {
 }
 
 # Recursive Find in a List
+# x <- list(d = list(c = list(a = "aa", b = "bb")), e = list(f = NA))
+# rfind(x, "a")
+# rfind(x, "f")
 rfind <- function(x, name, pos=c()) {
   ind <- match(name, names(x))
   if (!is.na(ind)) return(c(pos, ind))
@@ -17,3 +20,22 @@ rfind <- function(x, name, pos=c()) {
     }
   }
 }
+
+# Recursively Merge two Lists
+# x <- list(d = list(c = list(a = "aa", b = "bb")))
+# y <- list(d = list(c = list(a = "AA", c = "CC")))  # dominant
+# merge_list2(x, y)
+merge_list2 <- function(x, y) {
+  if (!is.list(x)) {
+    return(y)
+  }
+  # if list
+  nn <- intersect(names(y), names(x))
+
+  new.from.y <- y[!(names(y) %in% nn)]
+  z <- x
+  z[names(new.from.y)] <- new.from.y
+  z[nn] <- Map(merge_list2, x = x[nn], y = y[nn])
+  z
+}
+
