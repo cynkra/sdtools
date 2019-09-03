067a0a167363ee9a7d27f8823a78cab2ecab0e41
KarolisKoncevicius <karolis.koncevicius@gmail.com>
2019-09-03 12:15:25 +0300

    added dim_rename to dim operations

--- R/dim_add.R
+++ R/dim_add.R
@@ -1,20 +1,23 @@
-#' Add or Drop Dimensions
+#' Swissdata Dimension Operations
 #'
-#' `dim_add` adds an additional dimension to a Swissdata object, `dim_drop`
-#' removes a dimension.
+#' A set of functions for manipulating dimensions of swissdata objects.
 #'
-#' Given an existing Swissdata object and additional parameters of
-#' dimension name, dimension level, and dimension label this function
-#' incorporates the newly specified dimension by altering the `x$data`,
-#' `x$meta$hierarchy`, `x$meta$labels`, and \code{x$meta$dim_order`
-#' fields. When removing the dimesnion from `x$data` only the rows
-#' corresponding to provided level argument are retained
+#' `dim_add` adds an additional dimension to a swissdata object, `dim_drop`
+#' removes a dimension, `dim_rename` renames an existing dimension.
+#'
+#' Internally these functions incorporate the requested changes into a given
+#' swissdata object by altering the `x$data`, `x$meta$hierarchy`,
+#' `x$meta$labels`, `x$meta$units`, and `x$meta$dim_order` fields.
+#'
+#' When removing the dimesnion using `dim_drop` only the rows corresponding to
+#' provided level argument are retained in `x$data`. `dim_rename` changes the
+#' given dimension name
 #'
 #' @param x swissdata object
-#' @param dim name of dimension to be added or removed
-#' @param level single level ID to be used in the new dimension (`dim_add`), or
-#'   single level ID for which the data is filtered (`dim_drop`).
+#' @param dim dimension name
+#' @param level single level ID from `dim`
 #' @param label English label for the newly added dimension
+#' @param name new dimension name
 #'
 #' @return a modified swissdata object
 #'
@@ -27,6 +30,13 @@
 #' z$meta$labels$new_dim
 #' z$meta$hierarchy$new_dim
 #'
+#' # rename the newly added dimension to "dim2"
+#' z <- dim_rename(z, dim = "new_dim", name = "dim2")
+#' z$data
+#' z$meta$dim_order
+#' z$meta$labels$dim2
+#' z$meta$hierarchy$dim2
+#'
 #' # drop existing dimension idx_type (collapse to level "sch")
 #' z <- dim_drop(z, dim = "idx_type", level = "sch")
 #' z$data
@@ -35,6 +45,7 @@
 #' z$meta$hierarchy$idx_type
 #'
 #' @author Christoph Sax
+#' @name dim_operations
 #' @export
 dim_add <- function(x, dim = "geo", level = "0", label = level) {
   stopifnot(all(names(x$data) != dim))
@@ -49,7 +60,7 @@ dim_add <- function(x, dim = "geo", level = "0", label = level) {
 }
 
 
-#' @name dim_add
+#' @rdname dim_operations
 #' @export
 dim_drop <- function(x, dim = "trans", level = "ind") {
   stopifnot(level %in% x$data[[dim]])
@@ -68,6 +79,21 @@ dim_drop <- function(x, dim = "trans", level = "ind") {
     x$meta$units$all <- x$meta$units[[dim]][[level]]
     x$meta$units[dim] <- NULL
   }
+  x
+}
+
+
+#' @rdname dim_operations
+#' @export
+dim_rename <- function(x, dim, name) {
+  stopifnot(dim %in% setdiff(names(x$data), c("date", "value")))
+
+  x$meta$dim_order <- replace(x$meta$dim_order, x$meta$dim_order==dim, name)
+  names(x$data) <- replace(names(x$data), names(x$data)==dim, name)
+  names(x$meta$hierarchy) <- replace(names(x$meta$hierarchy), names(x$meta$hierarchy)==dim, name)
+  names(x$meta$labels) <- replace(names(x$meta$labels), names(x$meta$labels)==dim, name)
+  names(x$meta$units) <- replace(names(x$meta$units), names(x$meta$units)==dim, name)
+
   x
 }
 
