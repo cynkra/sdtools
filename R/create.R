2f49b5b96e215781dcfcab5a8944aa3e668ae48e
Christoph Sax <christoph.sax@gmail.com>
2020-04-18 09:13:30 +0200

    creator

---
+++ R/create.R
@@ -0,0 +1,33 @@
+#' Create Swissdata Objects
+#'
+#' Creator funtion for swissdata objects. All  swissdata objects should be
+#' created by this function.
+#'
+#' @param data data
+#' @param meta meta
+#' @param test should `dataset_validate()` be run on the newly-read object (default = TRUE)
+#'
+#' @return An object of class swissdata
+#'
+#' @examples
+#'
+#' dataset_create(adecco$data, adecco$meta, dataset_id = "ch_adecco_sjmi")
+#' @export
+dataset_create <- function(data, meta, dataset_id, test = TRUE) {
+
+  # use seco style labeling and usage of .
+  names(data) <- gsub(".", "_", names(data), fixed = TRUE)
+
+  z <- list(
+    meta = dots_to_underscore(empty_list_to_null(meta)),
+    data = data,
+    set_id = gsub(".", "_", dataset_id, fixed = TRUE)
+  )
+  names(z$meta) <- gsub("utc_updated", "updated_utc", names(z$meta), fixed = TRUE)
+
+  class(z) <- "swissdata"
+
+  if (test) ans <- dataset_validate(z)
+  z
+
+}
